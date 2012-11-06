a34e48a572314161393c7610df684a78c3ef5850
benbillbob <ben@benbillbob.com>
2012-11-07 00:12:48 +1100

    Initial Unit Test

---
+++ BusinessManagerTest/ViewModels/MainMenuViewModel_Test.cs
@@ -0,0 +1,38 @@
+﻿using BusinessManager.Framework;
+using BusinessManager.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BusinessManagerTest.ViewModels
+{
+    [TestFixture]
+    public class MainMenuViewModel_Test
+    {
+        MainMenuViewModel vm;
+
+        [SetUp]
+        public void Setup()
+        {
+            vm = new MainMenuViewModel();
+        }
+
+        [Test]
+        public void Constructor()
+        {
+            Assert.That(vm, Is.Not.Null);
+        }
+
+        [Test]
+        public void StudentListCommand()
+        {
+            var cmd = vm.StudentListCommand;
+            Assert.That(cmd, Is.Not.Null);
+            Assert.That(cmd, Is.TypeOf(typeof(RelayCommand)));
+        }
+    }
+}
