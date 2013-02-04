07c6bdbe7f10033faa140545eef134a3d161834f
Ben Woodcock <ben@benbillbob.com>
2013-02-05 00:18:27 +1100

    importing nearly

--- BusinessManager/ViewModels/StudentListViewModel.cs
+++ BusinessManager/ViewModels/StudentListViewModel.cs
@@ -16,6 +16,7 @@ using BusinessManager.Views;
 using Microsoft.Practices.Unity;
 using System.Windows.Data;
 using System.IO;
+using System.Data.Entity.Validation;
 
 namespace BusinessManager.ViewModels
 {
@@ -164,23 +165,141 @@ namespace BusinessManager.ViewModels
 
 		void parseString(string str)
 		{
-			var fields = str.Split('|');
+			var lines = str.Split(new string[] {"\r\n"}, StringSplitOptions.None);
 
-			var fieldNum = 56;
-			var idx = 0;
-			while (idx < (fields.Length / fieldNum))
+			var context = Container.Current.Resolve<IBusinessManagerEntities>();
+
+			for (var i = 0; i < lines.Length; i++)
 			{
-				var rec = fields.Skip(idx).Take(fieldNum);
-				var student = createStudent(rec.ToArray());
-				idx += fieldNum;
+				var rec = lines[i].Split('|');
+				if (rec.Length == 1)
+				{
+					continue;
+				}
+
+				var student = createStudent(rec);
+				context.AddStudent(student);
+			}
+
+			try
+			{
+				context.Save();
+			}
+			catch (DbEntityValidationException ex)
+			{
+				MessageBox.Show(ex.Message);
 			}
 		}
 
 		Student createStudent(string[] rec)
 		{
 			var student = new Student();
-			student.FirstName = rec[3];
+
+			student.Id = Guid.NewGuid();
+			student.FirstName = rec[(int)Fields.FirstName];
+			student.LastName = rec[(int)Fields.LastName];
+			decimal tmp = 0;
+			if (decimal.TryParse(rec[(int)Fields.Age], out tmp))
+			{
+				student.Age = tmp;
+			}
+
+			DateTime tmpDate;
+			if (DateTime.TryParse(rec[(int)Fields.DOB], out tmpDate))
+			{
+				student.DOB = tmpDate;
+			}
+
+			student.Sex = rec[(int)Fields.Sex];
+			student.School = rec[(int)Fields.School];
+			student.ParentFirstName = rec[(int)Fields.ParentFirstName];
+			student.ParentLastName = rec[(int)Fields.ParentLastName];
+			student.Address = rec[(int)Fields.Addr1] + " " + rec[(int)Fields.Addr2] + " " + rec[(int)Fields.Addr3] + " " + rec[(int)Fields.Addr4];
+			if (decimal.TryParse(rec[(int)Fields.PostCode], out tmp))
+			{
+				student.Postcode = tmp;
+			} 
+			student.HomePhone = rec[(int)Fields.HomePhone];
+			student.WorkPhone = rec[(int)Fields.WorkPhone];
+			student.MobilePhone = rec[(int)Fields.MobilePhone];
+			student.Email = rec[(int)Fields.Email];
+			student.Advertisement = rec[(int)Fields.Advertisement];
+			//student.AuditionComments = rec[(int)Fields.AuditionComments];
+			//student.Comments = rec[(int)Fields.Comments];
+			//student.SpecialNeeds = rec[(int)Fields.SpecialNeeds];
+			student.EmergencyContact1Name = rec[(int)Fields.EmergName1];
+			student.EmergencyContact1HomePhone = rec[(int)Fields.EmergHomePhone1];
+			student.EmergencyContact1WorkPhone = rec[(int)Fields.EmergWorkPhone1];
+			student.EmergencyContact1MobilePhone = rec[(int)Fields.EmergMobilePhone1];
+			student.EmergencyContact1RelationShip = rec[(int)Fields.EmergRel1];
+			student.EmergencyContact2Name = rec[(int)Fields.EmergName2];
+			student.EmergencyContact2HomePhone = rec[(int)Fields.EmergHomePhone2];
+			student.EmergencyContact2WorkPhone = rec[(int)Fields.EmergWorkPhone2];
+			student.EmergencyContact2MobilePhone = rec[(int)Fields.EmergMobilePhone2];
+			student.EmergencyContact2Relationship = rec[(int)Fields.EmergRel2];
+			student.ShirtSize = rec[(int)Fields.ShirtSize];
+
 			return student;
+		}
+
+		enum Fields
+		{
+			PrimaryKey = 0,
+			RecId,
+			StudentId,
+			FirstName,
+			LastName,
+			Age,
+			DOB,
+			Sex,
+			School,
+			ParentFirstName,
+			ParentLastName,
+			Addr1,
+			Addr2,
+			Addr3,
+			Addr4,
+			PostCode,
+			HomePhone,
+			WorkPhone,
+			MobilePhone,
+			Email,
+			MusicalExp,
+			Advertisement,
+			AuditionComments,
+			Level,
+			LevelOld,
+			FullPayment,
+			FullPaymentOld,
+			FirstPayment,
+			FirstPaymentOld,
+			SecondPayment,
+			SecondPaymentOld,
+			ThirdPayment,
+			ThirdPaymentOld,
+			FourthPayment,
+			FourthPaymentOld,
+			Comments,
+			SpecialNeeds,
+			EnrolmentReceived,
+			EnrolmentReceivedOld,
+			EmergName1,
+			EmergHomePhone1,
+			EmergWorkPhone1,
+			EmergMobilePhone1,
+			EmergRel1,
+			EmergName2,
+			EmergHomePhone2,
+			EmergWorkPhone2,
+			EmergMobilePhone2,
+			EmergRel2,
+			ShirtSize,
+			ShirtSizeOld,
+			ShirtReceived,
+			ShirtReceivedOld,
+			EmailContact,
+			PostContact,
+			TrialTime
 		}
 	}
 }
